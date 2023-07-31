3f7123b34b7e8a59d453ed0a47ff9bd840a4c5d1
Underscore96 <ottonelloiisabella@gmail.com>
2023-07-31 12:59:35 +0200

    cc

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+
+function calculateResult(){
+    const inputValue = document.getElementById('number').value;
+    const number = parseFloat(inputValue);
+    workerCalculus = (number);
+    // const resultSpan = document.getElementById('result');
+    // resultSpan.innerText = result;
+    const resultSpan = document.getElementById('result');
+    resultSpan.innerText = 'sto lavorando';
+
+
+}
+
+
+
+function workerCalculus(nbr){
+    const worker = new Worker('./lavoratore-salariato.js');
+    worker.postMessage(nbr)
+
+    worker.onmessage = (msg) => {
+        const resultSpan = document.getElementById('result');
+        resultSpan.innerText =msg.data;
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+function strangeCalculus(nbr){
+    let res = 0;
+
+    for (let i = 0; i <= nbr; i++) {
+        if (i % 2 ===  0) {
+            res += i;
+            
+        }
+        if(i % 3 === 0) {
+            res -= 1;
+            
+        }
+        
+    }
+
+
+    return res;
+}
+
